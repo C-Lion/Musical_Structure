105052e556e1b3c52458b45352fbfcf77eb13df1
C-Lion <esstepansky@gmail.com>
2019-07-19 17:55:10 +0300

    add intent to return home to main_activity from Media Category

--- app/src/main/java/com/example/musicplayer/MediaCategories.java
+++ app/src/main/java/com/example/musicplayer/MediaCategories.java
@@ -1,13 +1,31 @@
 package com.example.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MediaCategories extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Sets content of the activity to use activity_media_categories.xml layout file
         setContentView(R.layout.activity_media_categories);
+
+        //onClick listener for openPlayer activity
+        //First find the View that shows the Player activity link area:
+        TextView player = (TextView) findViewById(R.id.MediaBackToHome);
+        // Set a click listener on the player TextView
+        player.setOnClickListener(new View.OnClickListener() {
+            //The method that executes when the Play Now TextView is clicked on:
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/musicplayer/RecentlyPlayed.java
+++ app/src/main/java/com/example/musicplayer/RecentlyPlayed.java
@@ -1,7 +1,9 @@
 package com.example.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class RecentlyPlayed extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class RecentlyPlayed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recently_played);
+
+        //onClick listener for Return to Home Activity textview
+        //First find the View that shows the Player activity link area:
+        TextView player = (TextView) findViewById(R.id.MediaBackToHome);
+        // Set a click listener on the player TextView
+        player.setOnClickListener(new View.OnClickListener() {
+            //The method that executes when the Play Now TextView is clicked on:
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
